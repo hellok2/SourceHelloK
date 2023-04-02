459879ac528b1dc79fc2680da3e06e29efdfa4ce
Snoxy <damprog.one@gmail.com>
2023-04-02 12:33:10 +0200

    Added graphical interface

--- web-communicator/src/components/communicator/Communicator.js
+++ web-communicator/src/components/communicator/Communicator.js
@@ -3,21 +3,58 @@ import "./Communicator.css";
 
 function Communicator(){
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Learn React - from communicator
-                </a>
-            </header>
+        // <div className="App">
+        //     <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>
+        //         Edit <code>src/App.js</code> and save to reload.
+        //         </p>
+        //         <a
+        //         className="App-link"
+        //         href="https://reactjs.org"
+        //         target="_blank"
+        //         rel="noopener noreferrer"
+        //         >
+        //         Learn React - from communicator
+        //         </a>
+        //     </header>
+        // </div>
+
+        // communicator alias c
+        <div className="c_container">
+            <div className="c_messages">
+                <div className="c_message">
+                    <p className="c_guest-message">Hej, co słychać?</p>
+                </div>
+                <div className="c_message">
+                    <p className="c_own-message">Hej, u mnie super a u cb?</p>
+                </div>
+                <div className="c_message">
+                    <p className="c_guest-message">Hej, co słychać?</p>
+                </div>
+                <div className="c_message">
+                    <p className="c_own-message">Hej, u mnie super a u cb?</p>
+                </div>
+                <div className="c_message">
+                    <p className="c_guest-message">Hej, co słychać?</p>
+                </div>
+                <div className="c_message">
+                    <p className="c_own-message">Hej, u mnie super a u cb?</p>
+                </div>
+                <div className="c_message">
+                    <p className="c_own-message">Hej, u mnie super a u cb?</p>
+                </div>
+                <div className="c_message">
+                    <p className="c_own-message">Hej, u mnie super a u cb?</p>
+                </div>
+            </div>
+            <div className="c_create-message-area">
+                <div className="c_text-area">
+                    <input type="text"></input>
+                    {/* more in feature */}
+                </div>
+                <div className="c_send-btn">Send</div>
+            </div>
         </div>
     );
 }
